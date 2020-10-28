21bc88eb84d89ebf6a740d7dd8f822f70904fc3d
HDTran91 <KhoaiGa0103@gmail.com>
2020-10-27 21:14:38 -0500

    connect and Create Item on MongoDB and Create Environment File

--- src/server.js
+++ src/server.js
@@ -1,14 +1,30 @@
 var express = require("express");
+import ConnectDB from "./config/connectDB";
+import ContactModel from "./models/contact.model";
+
 var app = express();
 
-var hostname = "localhost";
-var port = 8017;
+//connect to MongoDB
+ConnectDB();
 
-app.get("/helloworld", (rerq, res) => {
-  res.send("<h1>Hello World !!! </h1>");
+// var hostname = "localhost";
+// var port = 8017;
+
+app.get("/test_database", async(req, res) => {
+  try {
+    let item = {
+      userID: "214124214",
+      contactID: "akjvjasviasoicaio",
+
+    };
+    let contact = await ContactModel.createNew(item);
+    res.send(contact);
+  }catch(err) {
+    console.log(err);
+  }
 
 });
 
-app.listen(port, hostname, () => {
-  console.log(`Hello Gacon, I' am running at ${hostname}:${port}/`);
+app.listen(process.env.APP_PORT, process.env.APP_HOST, () => {
+  console.log(`Hello Gacon, I' am running at ${process.env.APP_HOST}:${process.env.APP_PORT}/`);
 });
