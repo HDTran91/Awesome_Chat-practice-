effe5a76ba6a719ef40ad322a206e9d10f64d14e
HDTran91 <KhoaiGa0103@gmail.com>
2020-10-30 01:00:48 -0500

    add_router_and_controller

---
+++ src/controllers/authController.js
@@ -0,0 +1,13 @@
+let getLoginRegister = (req, res) => {
+  return res.render("auth/loginRegister");
+}
+
+let getLogout = (req,res) => {
+
+  //do something
+}
+
+module.exports = {
+  getLoginRegister: getLoginRegister,
+  getLogout: getLogout
+};
