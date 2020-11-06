f24375bcb49d2f079ba59c988ab9d11dc58d7105
HDTran91 <KhoaiGa0103@gmail.com>
2020-11-05 21:38:19 -0600

    Register_part_01

---
+++ lang/vi.js
@@ -0,0 +1,7 @@
+export const transValidation = {
+  email_incorrect: "Email phai co dang example@gacon.com!",
+  gender_incorrect: "Gender bi sai",
+  password_incorrect: "Mat Khau phai chua 8 ki tu bao gom chu hoang , chu thuong, chu so",
+  password_confirmation_incorrect: "mat khau khong trung khop",
+
+};
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,20 +1,18 @@
 import express from "express";
 import {home, auth} from "./../controllers/index"
+import {authValid} from "./../validation/index";
 
 let router = express.Router();
 
 /**
  * Init all routes
  *@param app from exactly express module
- */
+ */ 
 
  let initRoutes = (app) => {
   router.get("/", home.getHome);
-  
-  
-  
   router.get("/login-register", auth.getLoginRegister);
-  router.get("/logout", auth.getLogout);
+  router.post("/register", authValid.register, auth.postRegister);
   
 
 
