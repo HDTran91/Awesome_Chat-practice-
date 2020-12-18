263a63b588d82ebd1ae041f98cf1612ed8f462bc
HDTran91 <KhoaiGa0103@gmail.com>
2020-12-18 00:01:39 -0600

    login_by_facebook_account

--- src/server.js
+++ src/server.js
@@ -6,9 +6,16 @@ import bodyParser from "body-parser";
 import connectFlash from "connect-flash";
 import configSession from "./config/session";
 import passport from "passport";
+import pem from "pem";
+import https from "https"; //module trong node js
 
-//Init app
+pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
+  if (err) {
+  throw err;
+  }
+  //Init app
 let app = express();
+
 
 //connect to MongoDB
 ConnectDB();
@@ -34,10 +41,38 @@ app.use(connectFlash());
 app.use(passport.initialize());
 app.use(passport.session());
 
+
 //Init all routes
 initRoutes(app);
+https.createServer({ key: keys.serviceKey, cert: keys.certificate }, app).listen(process.env.APP_PORT,process.env.APP_HOST, () => {
+  console.log("hello Hoang, I am running at " + process.env.APP_HOST + ":" + process.env.APP_PORT +"/")
 
-
-app.listen(process.env.APP_PORT, process.env.APP_HOST, () => {
-  console.log(`Hello Gacon, I' am running at ${process.env.APP_HOST}:${process.env.APP_PORT}/`);
 });
+ 
+});
+// let app = express();
+
+// ConnectDB();
+
+// //config session
+
+// configSession(app);
+
+// //config view engine
+
+// configViewEngine(app);
+
+// //Enable post data for request
+
+// app.use(bodyParser.urlencoded({extended: true}))
+
+// //enable flash messages
+
+// app.use(connectFlash());
+
+// //config passport js
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// initRoutes(app);
